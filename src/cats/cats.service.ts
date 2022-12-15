9225fc7e6f72d7f9266b02376b40e1488053b266
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-12-15 13:53:57 +0000

    simplest possible project

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import * as mongo from 'mongodb';
+import { InjectDb } from 'nest-mongodb';
+import { Cat } from './interfaces/cats';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  private readonly collection: mongo.Collection;
+
+  constructor(@InjectDb() private readonly db: mongo.Db) {
+    this.collection = this.db.collection('cats');
+  }
+
+  async create(cat: Cat) {
+    const result = await this.collection.insertOne(cat);
+
+    return result;
+  }
+}
